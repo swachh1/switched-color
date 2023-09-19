18aab25ee28b9d76c97c2a9ffcd4ac0acf36c036
swachh1 <144605643+swachh1@users.noreply.github.com>
2023-09-19 12:37:44 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  //რენნდომ ნამბერი 0-3 მდე უნდა იყოს რადგან 4 ფერი გვაქვს
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
